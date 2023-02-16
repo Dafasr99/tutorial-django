64af0eb80fd3957991b22ca7b1d5899b49ed7235
Dafasr99 <anandafa.syukur@ui.ac.id>
2023-02-16 17:01:46 +0700

    Deployment

---
+++ README.md
@@ -0,0 +1,7 @@
+Nama : Anandafa Syukur Rizky
+
+NPM : 2106655040
+
+Kelas : Z
+
+Hobi : Tidur
---
+++ money_tracker/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from money_tracker.models import TransactionRecord
+
+# Create your views here.
+
+def show_tracker(request):
+
+    transaction_data = TransactionRecord.objects.all()
+    context = {
+        'list_of_transactions': transaction_data,
+        'name': 'Anandafa'
+    }
+    
+    return render(request, "tracker.html", context)
+
+
+
